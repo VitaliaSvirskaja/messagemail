b70127509316df250e378893750be4dfbd84d444
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-08-20 18:21:24 +0200

    Added features

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -1,5 +1,29 @@
+import styles from "../styles/features.module.css";
+
 interface Props {}
 
 export const Features = (props: Props) => {
-  return <div>Features</div>;
+  return (
+    <div className={styles.featureContainer}>
+      <div className={styles.feature}>
+        <div className={styles.featureBox}>
+          <p className={styles.featureText}>
+            Read & respond to DM’s from your social media accounts.
+          </p>
+        </div>
+      </div>
+      <div className={styles.feature}>
+        <div className={styles.featureBox}>
+          <p className={styles.featureText}>
+            Custom auto-response messages based on keywords from incoming DMs
+          </p>
+        </div>
+      </div>
+      <div className={styles.feature}>
+        <div className={styles.featureBox}>
+          <p className={styles.featureText}>Mass DM selected recipients</p>
+        </div>
+      </div>
+    </div>
+  );
 };
