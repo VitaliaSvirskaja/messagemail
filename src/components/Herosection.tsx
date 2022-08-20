b57b58ef3dc601942fd8379d85ad16d03b07d8e5
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-08-20 16:34:52 +0200

    Added herosection to homepage

---
+++ src/components/Herosection.tsx
@@ -0,0 +1,27 @@
+import banner from "../assets/hero.png";
+import styles from "../styles/herosection.module.css";
+import { Link } from "react-router-dom";
+import send from "../assets/send.png";
+
+interface Props {}
+
+export const Herosection = (props: Props) => {
+  return (
+    <div className={styles.herosection}>
+      <div>
+        <h1 className={styles.headline}>Manage your DM's with Ease</h1>
+        <h2 className={styles.subheadline}>
+          We offer you a central location to manage all of your social media
+          account’s direct messages!
+        </h2>
+        <Link to="/account">
+          <button className={styles.cta}>
+            <a className={styles.ctaText}>Manage my DM's</a>
+            <img src={send} alt="Send-Icon" className={styles.ctaIcon} />
+          </button>
+        </Link>
+      </div>
+      <img src={banner} alt="Hero-Banner" className={styles.banner} />
+    </div>
+  );
+};
