e92587f880a95ec0f509508e3ff6a42cc0be3ce1
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-08-22 21:22:47 +0200

    created "Create an Account" page with initial styles

--- src/pages/CreateAccountPage.tsx
+++ src/pages/CreateAccountPage.tsx
@@ -1,5 +1,36 @@
+import styles from "./CreateAccount.module.css";
+
 interface Props {}
 
 export const CreateAccountPage = (props: Props) => {
-  return <div>Create an account</div>;
+  return (
+    <div className={styles.content}>
+      <div className={styles.contentContainer}>
+        <div className={styles.headerContainer}>
+          <h1 className={styles.accountHeader1}>Create an account</h1>
+          <h2 className={styles.accountHeader2}>
+            After you create your basic account information, you will then be
+            asked to connect your various social media accounts.
+          </h2>
+        </div>
+        <div className={styles.accountForm}>
+          <div className={styles.formField}>
+            <label htmlFor="Name">Name</label>
+            <input type="text" />
+          </div>
+          <div className={styles.formField}>
+            <label htmlFor="Email Adress">Email Adress</label>
+            <input type="text" />
+          </div>
+          <div className={styles.formField}>
+            <label htmlFor="Name">Password</label>
+            <input type="password" />
+          </div>
+          <button className={styles.submitButton} type="submit">
+            Next
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
