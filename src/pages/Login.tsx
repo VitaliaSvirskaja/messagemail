56a80fce1fb1a76b0369d1fcf29f919e3791e4cc
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-08-25 09:27:23 +0200

    Added Login component & page

---
+++ src/pages/Login.tsx
@@ -0,0 +1,51 @@
+import styles from "./Login.module.css";
+import { FormField } from "../components/shared/FormField";
+import { useState } from "react";
+import eyeOffIcon from "../assets/eye-off.png";
+import eyeIcon from "../assets/eye.png";
+interface Props {}
+
+export const Login = (props: Props) => {
+  const [passwordVisible, setPasswordVisible] = useState<boolean>(false);
+
+  function handleEyeclick() {
+    setPasswordVisible(!passwordVisible);
+  }
+
+  return (
+    <div className={styles.contentContainer}>
+      <div className={styles.loginContainer}>
+        <h1 className={styles.headerLogin}>Welcome back!</h1>
+        <div className={styles.formContainer}>
+          <div>
+            {" "}
+            <FormField type={"text"} label={"Email Adress"} />
+          </div>
+          <div>
+            <FormField
+              type={passwordVisible ? "text" : "password"}
+              label={"Password"}
+            />
+
+            <img
+              src={passwordVisible ? eyeOffIcon : eyeIcon}
+              alt="Eye-Icon"
+              className={styles.eyeIcon}
+              onClick={handleEyeclick}
+            />
+            <div className={styles.forgotPassword}>Forgot passwort</div>
+          </div>
+          <button className={styles.submitButton} type="submit">
+            Login
+          </button>
+        </div>
+      </div>
+      <div className={styles.registerContainer}>
+        <h2 className={styles.headerRegister}>New here?</h2>
+        <a href="/account">
+          <button className={styles.registerButton}>Create account</button>
+        </a>
+      </div>
+    </div>
+  );
+};
