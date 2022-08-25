eef0ffcb88bab26233a7571f774bee9cd56f1d2e
Vitalia Svirskaja <vitalia.svirskaja.vs@gmail.com>
2022-08-25 14:53:40 +0200

    Refactored feature section on homepage

--- src/components/home/Features.tsx
+++ src/components/home/Features.tsx
@@ -1,4 +1,7 @@
 import styles from "./Features.module.css";
+import feature1 from "../../assets/feature1.png";
+import feature2 from "../../assets/feature2.png";
+import feature3 from "../../assets/feature3.png";
 
 interface Props {}
 
@@ -6,16 +9,19 @@ export const Features = (props: Props) => {
   return (
     <div className={styles.featureContainer}>
       <div className={styles.featureBox}>
+        <img src={feature1} alt="Feature1" className={styles.featureIcon} />
         <p className={styles.featureText}>
           Read & respond to DM’s from your social media accounts.
         </p>
       </div>
       <div className={styles.featureBox}>
+        <img src={feature2} alt="Feature2" className={styles.featureIcon} />
         <p className={styles.featureText}>
           Custom auto-response messages based on keywords from incoming DMs
         </p>
       </div>
       <div className={styles.featureBox}>
+        <img src={feature3} alt="Feature3" className={styles.featureIcon} />
         <p className={styles.featureText}>Mass DM selected recipients</p>
       </div>{" "}
     </div>
